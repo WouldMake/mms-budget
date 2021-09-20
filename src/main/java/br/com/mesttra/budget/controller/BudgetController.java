5a0b31788b0e52f632410875f16cfc594a4dbe0e
Marcelo Faria <marcelo.fari@gmail.com>
2021-09-20 17:55:53 -0300

    Inclusão de documentação com o Swagger.
     Changes to be committed:
    	modified:   pom.xml
    	new file:   src/main/java/br/com/mesttra/budget/config/SpringFoxConfig.java
    	modified:   src/main/java/br/com/mesttra/budget/controller/BudgetController.java

--- src/main/java/br/com/mesttra/budget/controller/BudgetController.java
+++ src/main/java/br/com/mesttra/budget/controller/BudgetController.java
@@ -4,6 +4,9 @@ import br.com.mesttra.budget.exception.BusinessException;
 import br.com.mesttra.budget.model.Budget;
 import br.com.mesttra.budget.request.DebitExpenseRequest;
 import br.com.mesttra.budget.service.BudgetService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,9 +23,20 @@ public class BudgetController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Budget addBudget(@Valid @RequestBody Budget budget) { return budgetService.addBudget(budget); }
+    @ApiOperation(value = "Add budget",
+            notes = "This method adds a new budget.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Budget added"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })    public Budget addBudget(@Valid @RequestBody Budget budget) { return budgetService.addBudget(budget); }
 
     @GetMapping
+    @ApiOperation(value = "List budgets",
+            notes = "This method lists all budgets.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Budgets listed"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public List<Budget> listBudgets(@RequestParam(required = false) String destination) {
 
         if (destination == null || destination.isEmpty())
@@ -32,6 +46,12 @@ public class BudgetController {
     }
 
     @PatchMapping("/{id}/expense")
+    @ApiOperation(value = "Debit an expense from an existing budget",
+            notes = "This method finds a budget by its id and debits an expense from it.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Budget changed"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public Budget debitExpense(@PathVariable Long id, @Valid @RequestBody DebitExpenseRequest debitExpenseRequest) throws BusinessException {
         return  budgetService.debitExpense(id, debitExpenseRequest);
     }
